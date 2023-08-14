e5e1eaea30c57ffcb74c5b81d1090ff865149d18
Bharath raj <116830055+bharathraj1155@users.noreply.github.com>
2023-08-14 22:46:43 +0530

    Add files via upload

---
+++ responses.js
@@ -0,0 +1,29 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    }
+      else if (input == "your name?",) {
+        return "My name is chatbot. What is your name?";
+    }  else if (input == "I love You") {
+        return "You are gay";
+    }  else if (input == prompt()){
+        return "Haha what a silly name ";
+    }  else if (input == "Raghavendra"){
+        return "Nice name";
+    }  else {
+        return "kelambu da punda";
+    }
+    
+}
